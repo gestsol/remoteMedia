369e9996c47a64e4e47f2800af848ecc15fc8616
Dorian <dorian.celu@gmail.com>
2023-07-20 16:22:22 -0400

    test-a33

--- ejm.php
+++ ejm.php
@@ -1,6 +1,6 @@
 <?php
 
-include "/com/com.php" ;
+include "./com/com.php" ;
 
 $QRY="select * from codigos";
 
@@ -9,14 +9,14 @@ $res= mysqli_query($mysqli,$QRY);
 $data=mysqli_fetch_array($res);
 
 
-echo 
+echo
 $codigo=$data['codigo'];
 
 
 
-// header("Content-type: text/xml");
-// echo "<?xml version='1.0' encoding='UTF-8'?>";
-// echo "<note>";
-// echo "<message>$codigo</message>";
-// echo "</note>";
+header("Content-type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<note>";
+echo "<message>$codigo</message>";
+echo "</note>";
 ?>
--- responder.php
+++ responder.php
@@ -8,10 +8,14 @@ $res= mysqli_query($mysqli,$QRY);
 $data=mysqli_fetch_array($res);
 
 
-echo 
+ 
 $codigo=$data['codigo'];
 
-
+header("Content-type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<note>";
+echo "<message>$codigo</message>";
+echo "</note>";
 
 
 
