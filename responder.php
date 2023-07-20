4d2e7b551f1c1e7fd46629e4c3e537ebddb678cb
Dorian <dorian.celu@gmail.com>
2023-07-20 16:09:48 -0400

    test-f

--- responder.php
+++ responder.php
@@ -1,5 +1,5 @@
 <?php 
-include "./com/com.php" ;
+include "/com/com.php" ;
 
 $QRY="select * from codigos";
 
@@ -11,28 +11,17 @@ $data=mysqli_fetch_array($res);
 
 $codigo=$data['codigo'];
 
+$xml="<?xml version='1.0'  encoding='UTF-8'?>
+<content>
+  <text>
+    <content>$codigo</content>
+    <duration>5</duration>
+  </text>
+</content>";
 
+$carga_xml = simplexml_load_string($xml);
 
-header("Content-type: text/xml");
-echo "<?xml version='1.0' encoding='UTF-8'?>";
-echo "<note>";
-echo "<from>$codigo</from>";
-echo "<to>Tove</to>";
-echo "<message>Remember me this weekend</message>";
-echo "</note>";
-
-
-// $xml="<?xml version='1.0'  encoding='UTF-8'?>
-// <content>
-//   <text>
-//     <content>$codigo</content>
-//     <duration>5</duration>
-//   </text>
-// </content>";
-
-// $carga_xml = simplexml_load_string($xml);
-
-// var_dump($carga_xml);
+var_dump($carga_xml);
 
 
 
