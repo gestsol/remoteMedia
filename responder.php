f8c12185e9dff4534ee8b7f5a8d2ca79ff96a544
Dorian <dorian.celu@gmail.com>
2023-07-20 16:18:38 -0400

    test-g

--- responder.php
+++ responder.php
@@ -8,21 +8,8 @@ $res= mysqli_query($mysqli,$QRY);
 $data=mysqli_fetch_array($res);
 
 
-
+echo 
 $codigo=$data['codigo'];
-
-$xml="<?xml version='1.0'  encoding='UTF-8'?>
-<content>
-  <text>
-    <content>$codigo</content>
-    <duration>5</duration>
-  </text>
-</content>";
-
-$carga_xml = simplexml_load_string($xml);
-
-var_dump($carga_xml);
-
 
 
 
