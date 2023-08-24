de339f1c3e66e9058eb560c976e8c79725d2aa59
Dorian <dorian.celu@gmail.com>
2023-08-24 11:21:46 -0400

    RSS

---
+++ rss.php
@@ -0,0 +1,24 @@
+<?php
+// Conexión a la base de datos
+$conexion = mysqli_connect('localhost', 'usuario', 'contraseña', 'RSS');
+
+// Consulta para obtener los mensajes desde la base de datos
+$query = "SELECT * FROM mensajes";
+$resultado = mysqli_query($conexion, $query);
+
+// Creación del objeto XML para el feed RSS
+$feed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss></rss>');
+$feed->addChild('channel');
+
+// Recorrido de los resultados y creación de los elementos del feed RSS
+while ($fila = mysqli_fetch_assoc($resultado)) {
+    $item = $feed->channel->addChild('item');
+    $item->addChild('title', $fila['titulo']);
+    $item->addChild('description', $fila['descripcion']);
+    $item->addChild('pubDate', date('r', strtotime($fila['fecha'])));
+}
+
+// Encabezados y salida del feed RSS
+header('Content-Type: application/rss+xml; charset=utf-8');
+echo $feed->asXML();
+?>
